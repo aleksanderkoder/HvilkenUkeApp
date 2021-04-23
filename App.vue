a7fdb99d8ef7d599847aa3931c9ec9002eb0acd6
Aleksander Johansen <aleksander.johans@gmail.com>
2021-04-23 06:53:41 +0200

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,73 @@
+<template>
+  <view class="container">
+    <text class="textHeader">Vi er i uke</text>
+    <text class="text-color-primary">{{ message }}</text>
+    <text class="under">{{ date }}</text>
+    <button class="btn" v-bind:on-press="test" title="Oppdater"/>
+  </view>
+</template>
+
+<script>
+
+export default {
+  data: function () {
+    return {
+      message: "",
+      date: ""
+    }
+  },
+  methods: {
+    test () {
+      this.message = this.getWeek(); 
+    },
+    getWeek () {
+      Date.prototype.getWeek = function() {
+        var onejan = new Date(this.getFullYear(),0,1);
+        return Math.ceil((((this - onejan) / 86400000) + onejan.getDay()+1)/7);
+      }
+
+      var today = new Date();
+      var weekNumber = today.getWeek();
+      return weekNumber - 1; 
+    },
+    getDate () {
+      let date = new Date();
+      return date.getDate() + "." + parseInt(date.getMonth() + 1) + "." + date.getFullYear(); 
+    }
+  },
+  mounted () {
+    this.message = this.getWeek(); 
+    this.date = this.getDate();
+  }
+}
+</script>
+
+
+<style>
+
+.textHeader {
+  font-size: 26px; 
+}
+.container {
+  background-color: white;
+  align-items: center; 
+  justify-content: center;
+  flex: 1; 
+}
+
+.under {
+  padding-top: 10px;
+  padding-bottom: 65px;  
+}
+
+.btn {
+  position: absolute;
+  bottom: 0; 
+  left: 0;
+  right: 0;
+}
+
+.text-color-primary {
+  font-size: 110px;
+}
+</style>
